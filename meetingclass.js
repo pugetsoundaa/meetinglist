336c99eb41d15ca0678829deaf9040f7c058a2e6
mathteacher <contact@jacobelstein.com>
2017-05-19 11:24:24 -0700

    a couple tweeks and setup for fixing print issue

--- meetingclass.js
+++ meetingclass.js
@@ -60,7 +60,7 @@ class Meeting{
 		htmlstring = htmlstring+"<td data-value=\""+this.stime_num+"\">"+this.stime+"</td>"; // String start time with numeric data-value for sorting
 		htmlstring = htmlstring+"<td class=\"oc\">"+open_char+"</td><td>"+newmname+"</td>"; // O or C and New Name
 		// used apple maps, as if it's a non apple device it automatically goes to google maps
-		htmlstring = htmlstring+"<td><a target=\"_blank\" href=\"http://maps.apple.com/?q="+wholeaddress+"\">"+wholeaddress+"</a></td>";
+		htmlstring = htmlstring+"<td><a target=\"_blank\" href=\"http://maps.apple.com/?q="+wholeaddress+"\">"+this.address+" ["+this.zipcode+"]"+"</a></td>";
 		htmlstring = htmlstring+"<td>"+this.city+"</td>"; // city
 		htmlstring = htmlstring+"<td>"+newnotes+"</td></tr>"; // notes
 		
